be4efa73d90974405668aaed1d1c40d35e7c5203
Alim delibas <alim.delibass@gmail.com>
2024-02-10 21:35:12 +0300

    commited all features

--- newTableVProject/ViewController.swift
+++ newTableVProject/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tfMetin: UITextField!
+    
+    @IBOutlet weak var tvListe: UITableView!
+    var Liste = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnEkle(_ sender: Any) {
+        if tfMetin.text!.isEmpty == true{
+            let alertcontroller = UIAlertController(title: "Uyarı", message: "Boş bırakamazsınız lütfen bir değer giriniz", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "tamam", style: .default)
+            alertcontroller.addAction(alertAction)
+            present(alertcontroller, animated: true)
+        }else {
+            Liste.append(tfMetin.text!)
+            tvListe.reloadData()
+            
+           }
+       
+    }
+    
 }
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return Liste.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = Liste[indexPath.row]
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            Liste.remove(at: indexPath.row)
+            tvListe.reloadData()
+        }
+    }
+    
+}
+    
+    
+    
+    
 
