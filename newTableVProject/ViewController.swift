93c8158205fa1f7ccf5de18b82709f1f21156fa5
Alim delibas <alim.delibass@gmail.com>
2024-02-10 20:33:25 +0300

    Initial Commit

---
+++ newTableVProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newTableVProject
+//
+//  Created by alim on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
